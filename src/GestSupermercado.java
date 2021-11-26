e99fc723652e361c5dca3522397e5f317e3d2022
Vasques <nuno.a.vasques@outlook.com>
2021-11-26 18:01:43 +0000

    Leitura dos dados mais asthethic

--- src/GestSupermercado.java
+++ src/GestSupermercado.java
@@ -1,3 +1,4 @@
+import java.awt.print.Printable;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -8,7 +9,10 @@ public class GestSupermercado {
     private static ArrayList<Supermercado> supermercados = new ArrayList<>();
 
     public static void main(String[] args) {
+        System.out.println("Software Boot");
         lerdados();
+        System.out.println("Software up to date");
+
         Supermercado Sup= EscolherSupermercado();
         Cliente cliente=null;
         while(cliente==null) {
@@ -26,35 +30,31 @@ public class GestSupermercado {
             ArrayList<Produto> produtos = new ArrayList<>();
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
-                if(data.charAt(0)=='!'){
+                if(data.contains("*NOME*")){
                     mododeescrita=0;
-                    data=data.replaceFirst("! ","");
                 }
-                else if(data.charAt(0)=='-') {
+                else if(data.contains("*PRODUTOS*")) {
                     mododeescrita = 1;
-                    data=data.replaceFirst("- ","");
                 }
-                else if(data.charAt(0)=='>'){
+                else if(data.contains("*CLIENTES*")){
                     mododeescrita=2;
-                    data=data.replaceFirst("> ","");
                 }
-                else if(data.charAt(0)=='/')
-                    mododeescrita=3;
-
-                switch (mododeescrita){
-                    case 0->nome=data;
-                    case 1->{
-                        String[] array=data.split(" ");
-                        produtos.add(new Produto(array[0],array[1],Float.parseFloat(array[2]),Integer.parseInt(array[3]),Integer.parseInt(array[4])));
-                    }
-                    case 2->{
-                        String[] array=data.split(" ");
-                        clientes.add(new Cliente(array[0],array[1],array[2],Integer.parseInt(array[3]),new Data(Integer.parseInt(array[4]),Integer.parseInt(array[5]),Integer.parseInt(array[6])),Boolean.parseBoolean(array[7])));
-                    }
-                    case 3->{
-                        supermercados.add(new Supermercado(nome,clientes,produtos));
-                        clientes = new ArrayList<>();
-                        produtos = new ArrayList<>();
+                else if(data.contains("*PUSH*")) {
+                    supermercados.add(new Supermercado(nome, clientes, produtos));
+                    clientes = new ArrayList<>();
+                    produtos = new ArrayList<>();
+                }
+                else {
+                    switch (mododeescrita) {
+                        case 0 -> nome = data;
+                        case 1 -> {
+                            String[] array = data.split(" ");
+                            produtos.add(new Produto(array[0], array[1], Float.parseFloat(array[2]), Integer.parseInt(array[3]), Integer.parseInt(array[4])));
+                        }
+                        case 2 -> {
+                            String[] array = data.split(" ");
+                            clientes.add(new Cliente(array[0], array[1], array[2], Integer.parseInt(array[3]), new Data(Integer.parseInt(array[4]), Integer.parseInt(array[5]), Integer.parseInt(array[6])), Boolean.parseBoolean(array[7])));
+                        }
                     }
                 }
 
